6a420bbd1b055c8f50a624bd3245d1997bd269a5
Shashwat Dalakoti <shash.dal623@gmail.com>
2022-07-29 00:14:38 +0530

    Update ParkingManagement.java

--- src/main/java/bike/rapido/paathshala/parkinglot/ParkingManagement.java
+++ src/main/java/bike/rapido/paathshala/parkinglot/ParkingManagement.java
@@ -1,16 +1,26 @@
 package bike.rapido.paathshala.parkinglot;
 
 public class ParkingManagement {
-    static int availableSlot = 4;
+    private static int availableSlot = 4;
 
 
-    public String canPark() {
+    public String park() {
         if(availableSlot>0){
             availableSlot=availableSlot-1;
             return "You Can Park";
         }
         else{
             return "Parking Full";
+        }
+    }
+
+    public String depart(){
+        if(availableSlot < 4){
+            availableSlot = availableSlot + 1;
+            return "Car Departed";
+        }
+        else{
+            return "Parking Empty";
         }
     }
 
